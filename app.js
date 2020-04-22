fb7129e1b7a16a4faf07c02e1924871a952885fd
Edoardo <evicenzi357@gmail.com>
2020-04-22 17:10:03 +0200

    Base Version 0.0.0

--- app.js
+++ app.js
@@ -1,7 +1,50 @@
-new Vue({
+Vue.component('home-top-bar-small', {
 
-  el: '#vue-app',
-  data: {
-    name: 'Edoardo'
-  }
+  template :
+  `<div>
+
+    <img alt="back" src="./assets/images/baseline_navigate_before_black_48dp.png" style="width:36px;height:36px;">
+
+
+
+
+  </div>`
+
+
+});
+
+
+Vue.component('', {
+
+  template : `<div>
+
+  </div>`
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+new Vue({
+  el  : '#vue-app',
+
 });
